f833cf5ad1331056cf5132da949bcc9ee3398e09
Malsha Rukshani <151753061+MalshaRukshani@users.noreply.github.com>
2024-12-02 18:07:44 +0530

    Update Sorting.java

--- Sorting.java
+++ Sorting.java
@@ -77,34 +77,7 @@ public class Sorting {
         return i + 1;
     }
 
-    public void heapSort(double[] array) {
-        int n = array.length;
-        for (int i = n / 2 - 1; i >= 0; i--) {
-            heapify(array, n, i);
-        }
-        for (int i = n - 1; i > 0; i--) {
-            double temp = array[0];
-            array[0] = array[i];
-            array[i] = temp;
-            heapify(array, i, 0);
-        }
-    }
 
-    private void heapify(double[] array, int n, int i) {
-        int largest = i;
-        int left = 2 * i + 1;
-        int right = 2 * i + 2;
-
-        if (left < n && array[left] > array[largest]) largest = left;
-        if (right < n && array[right] > array[largest]) largest = right;
-
-        if (largest != i) {
-            double swap = array[i];
-            array[i] = array[largest];
-            array[largest] = swap;
-            heapify(array, n, largest);
-        }
-    }
 
 }
 
